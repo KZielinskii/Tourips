bd2d1efdd5c900ef9b44765e310c2c0129f3c4c0
KZielinskii <58587948+KZielinskii@users.noreply.github.com>
2023-12-19 20:05:49 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:tourpis/screens/signin_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-Future<void> main()  async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    print('Error initializing Firebase: $e');
+  }
+
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});

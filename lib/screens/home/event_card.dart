d391ca01d23d0bfb7d0f372d45e788dbf82c5315
KZielinskii <58587948+KZielinskii@users.noreply.github.com>
2024-01-18 00:53:56 +0100

    improved appearance of cards

--- lib/screens/home/event_card.dart
+++ lib/screens/home/event_card.dart
@@ -11,19 +11,43 @@ class EventCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.blue,
+      color: Colors.black54,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: ListTile(
-        title: Text(
-          event.title,
-          style: const TextStyle(color: Colors.white),
-        ),
-        subtitle: Text(
-          event.description,
-          style: const TextStyle(color: Colors.white),
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              event.title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Początek wydarzenia:\n${event.startDate.day}/${event.startDate.month}/${event.startDate.year} ${event.startDate.hour}:${event.startDate.minute}",
+                  style: const TextStyle(color: Colors.white),
+                ),
+                Text(
+                  "\nMiejsca: ${event.participants}/${event.capacity}",
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/home/event_card_with_button.dart
+++ lib/screens/home/event_card_with_button.dart
@@ -16,35 +16,60 @@ class EventCardWithButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.blue,
+      color: Colors.black54,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: ListTile(
-        title: Text(
-          event.title,
-          style: const TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              event.description,
-              style: const TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 8),
-            Align(
-              alignment: Alignment.centerRight,
-              child: ElevatedButton(
-                onPressed: isButtonEnabled ? handleJoinRequest : null,
-                child: const Text("Poproś o dołączenie"),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              event.title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
               ),
             ),
-          ],
-        ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 16, 0),
+                child: Text(
+                  "\nMiejsca: ${event.participants}/${event.capacity}",
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Początek wydarzenia:\n${event.startDate.day}/${event.startDate.month}/${event.startDate.year} ${event.startDate.hour}:${event.startDate.minute}",
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      onPressed: isButtonEnabled ? handleJoinRequest : null,
+                      child: const Text(
+                        "Poproś o dołączenie",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ]),
+          ),
+        ],
       ),
     );
   }
